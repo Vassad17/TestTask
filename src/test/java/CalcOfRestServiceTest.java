8ad74f8006d345b793fc05a58e88359550b3774f
Vasiliy <Ruyk17@gmail.com>
2024-09-29 15:17:39 +0300

    ChangeInitial

--- src/test/java/CalcOfRestServiceTest.java
+++ src/test/java/CalcOfRestServiceTest.java
@@ -1,18 +1,16 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
 import ru.netology.services.CalcOfRestService;
 
 
 public class CalcOfRestServiceTest {
 
-    @Test
+    @ParameterizedTest
     void shouldtestCalcOfRestService(
     ) {
         CalcOfRestService calcOfRestService = new CalcOfRestService();
-
-        int income = 10_000;
-        int expenses = 3000;
         int expected = 9;
-        int actual = calcOfRestService.calculate(income, expenses);
+        int actual = calcOfRestService.calculate(10000, 3000);
     }
 }
